04e3a63319a027c356787b64488bcc4521c304f5
Momade Afonso Sarajabo Junior <momadejunior584@gmail.com>
2020-06-26 21:53:48 +0200

    sender

--- index.php
+++ index.php
@@ -12,20 +12,3 @@ $headers = 'From: '.$email. "\n";;
 mail($to,$subject,$txt,$headers);
 
 ?>
-
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>email</title>
-</head>
-<body>
-	<form method="post">
-		<input type="text" name="name" placeholder="nome"><br>
-		<input type="text" name="assunto" placeholder="assunto"><br>
-		<input type="text" name="email" placeholder="email"><br>
-		<textarea type="text" name="mensagem" placeholder="email" row="5"></textarea><br>
-		<input type="submit" name="submit" value="enviar">
-	</form>
-</body>
-</html>
